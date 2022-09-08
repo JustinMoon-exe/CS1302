cff7b6426c273a66bcf410cfce0e90291734f86d
JustinMoon-exe <jjmoonjeli@gmail.com>
2022-09-08 18:01:18 -0400

    lab 2 temp

--- CS LAB 1/src/MatrixTransposeLab.java
+++ CS LAB 1/src/MatrixTransposeLab.java
@@ -2,6 +2,7 @@ public class MatrixTransposeLab {
 	public static void main(String[] args) {
 
 		int[][] my2D_wide= {{21,32,23,34,25,56,88},{13,14,15,16,17,18,19}};
+		
 		System.out.println("Original Matrix: ");
 		PrintVect2D(my2D_wide);
 		int rows=0;
@@ -15,41 +16,59 @@ public class MatrixTransposeLab {
 
 		int[][] my2D_tall = new int[cols][rows];
 		my2D_tall=Transpose2D(my2D_wide);
+
+		int[][] matmult = new int[my2D_tall[0].length][my2D_wide.length];
 		
+		DotProd2D(my2D_wide, my2D_tall, matmult);
+
 		System.out.println("\nTransposed Matrix: ");
 		PrintVect2D(my2D_tall);
+		System.out.println("\nDot Product of Matricies: ");
+		PrintVect2D(matmult);
 		
-}
-public static int[][] Transpose2D(int[][] vect) {
-	//Uncomment the next two (2) lines of code to setup the 'trans' 2D-vector, properly
-	int cols = vect[0].length;
-	int rows = vect.length;
-	int trans[][] = new int[cols][rows];
+	}	
+	public static int[][] Transpose2D(int[][] vect) {
+		//Uncomment the next two (2) lines of code to setup the 'trans' 2D-vector, properly
+		int cols = vect[0].length;
+		int rows = vect.length;
+		int trans[][] = new int[cols][rows];
 
-	//use nested loops similar to the 'PrintVect2D' below that transpose vect into trans
-	for(int i = 0; i < rows; i++){
-		for (int j = 0; j < cols; j++) {
-			trans[j][i] = vect[i][j];
+		//use nested loops similar to the 'PrintVect2D' below that transpose vect into trans
+		for(int i = 0; i < rows; i++){
+			for (int j = 0; j < cols; j++) {
+				trans[j][i] = vect[i][j];
+			}
 		}
+
+		return trans;
 	}
 
-	return trans;
-}
-public static void PrintVect2D(int[][] vect) {
-	System.out.print("{ ");
-	for(int i=0; i<vect.length; i++) {
-		if(i>0)
-			System.out.print("  ");
-		for(int j=0; j<vect[0].length; j++) {
-			if(j<vect[0].length-1)
-				System.out.print(vect[i][j]+", ");
+	public static int[][] DotProd2D(int[][] wide, int[][] tall, int[][] matmult ) {
+		for(int i = 0; i < matmult.length; i++){
+			for (int j = 0; j < matmult[0].length; j++) {
+				matmult[i][j] = wide[i][j] * tall[j][i];
+			}
+		}
+		
+		return(matmult);
+	}
+
+
+	public static void PrintVect2D(int[][] vect) {
+		System.out.print("{ ");
+		for(int i=0; i<vect.length; i++) {
+			if(i>0)
+				System.out.print("  ");
+			for(int j=0; j<vect[0].length; j++) {
+				if(j<vect[0].length-1)
+					System.out.print(vect[i][j]+", ");
+				else
+					System.out.print(vect[i][j]);
+			}
+			if(i<vect.length-1)
+				System.out.println("");
 			else
-				System.out.print(vect[i][j]);
+				System.out.println(" }");
 		}
-		if(i<vect.length-1)
-			System.out.println("");
-		else
-			System.out.println(" }");
 	}
-}
 }
